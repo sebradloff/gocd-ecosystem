f548da2f705cc8894705edae6032a184c5efcbd4
Sebastian Radloff <sradloff23@gmail.com>
2017-05-14 18:26:23 -0400

    Volume mount plugins instead of copying them into the Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,1 @@
 FROM gocd/gocd-server:v17.4.0
-
-# add plugins
-RUN mkdir -p /godata/plugins/external/
-COPY plugins/*.jar /godata/plugins/external/
-RUN chown -R go:go /godata/plugins/external/
--- start_go_server.sh
+++ start_go_server.sh
@@ -2,4 +2,5 @@ docker build --tag sebastian-go-server .
 
 docker run --rm -p8153:8153 \
   -v $(pwd)/cruise-config.xml:/godata/config/cruise-config.xml \
+  -v $(pwd)/plugins:/godata/plugins/external/ \
   sebastian-go-server
